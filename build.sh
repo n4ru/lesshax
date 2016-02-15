0463134e870fbfe1567f40f69098f975c1a29a67
n4ru <admin@n4ru.it>
2016-02-15 13:19:09 -0500

    build 9.0

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+python scripts/buildVersion.py "9.0.0-20U"
+python scripts/buildVersion.py "N9.0.0-20U"
+python scripts/buildVersion.py "9.0.0-20E"
+python scripts/buildVersion.py "N9.0.0-20E"
+python scripts/buildVersion.py "9.0.0-20J"
+python scripts/buildVersion.py "N9.0.0-20J"
 python scripts/buildVersion.py "9.2.0-20U"
 python scripts/buildVersion.py "N9.2.0-20U"
 python scripts/buildVersion.py "9.2.0-20E"
 python scripts/buildVersion.py "N9.2.0-20E"
 python scripts/buildVersion.py "9.2.0-20J"
 python scripts/buildVersion.py "N9.2.0-20J"
+mv r/POST5_U_11272_8203.bin payloads/ropbinpayload_menuhax_USA11272_old3ds.bin
+mv r/POST5_E_11272_8203.bin payloads/ropbinpayload_menuhax_EUR11272_old3ds.bin
+mv r/POST5_J_11272_8203.bin payloads/ropbinpayload_menuhax_JPN11272_old3ds.bin
+mv r/N3DS_U_11272_8203.bin payloads/ropbinpayload_menuhax_USA11272_new3ds.bin
+mv r/N3DS_E_11272_8203.bin payloads/ropbinpayload_menuhax_EUR11272_new3ds.bin
+mv r/N3DS_J_11272_8203.bin payloads/ropbinpayload_menuhax_JPN11272_new3ds.bin
 mv r/POST5_U_12288_8203.bin payloads/ropbinpayload_menuhax_USA12288_old3ds.bin
 mv r/POST5_E_12288_8203.bin payloads/ropbinpayload_menuhax_EUR12288_old3ds.bin
 mv r/POST5_J_12288_8203.bin payloads/ropbinpayload_menuhax_JPN12288_old3ds.bin
